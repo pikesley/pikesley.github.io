e501493478188472c2b9efbd562321de7719a64d
pikesley <github@orgraphone.org>
2017-12-12 22:58:53 +0000

    jQuery spec

--- spec/pikesley_spec.rb
+++ spec/pikesley_spec.rb
@@ -1,17 +1,40 @@
-describe '/' do
-  it 'has the correct title' do
-    expect(subject).to contain """
-    <title>
-    sam.pikesley.org
-    </title>
-    """
+describe '_site' do
+  context 'root' do
+    it 'has the correct title' do
+      expect('/').to contain """
+      <title>
+      sam.pikesley.org
+      </title>
+      """
+    end
   end
-end
 
-describe '/cv/' do
-  it 'has the correct title' do
-    expect(subject).to contain """
-    My CV
-    """
+  context '/cv/' do
+    it 'has the correct title' do
+      expect('/cv/').to contain """
+      <title>
+      My CV
+      </title>
+      """
+    end
+
+    context 'jquery' do
+      it 'has the right toggle for moo' do
+        expect('/cv/').to contain """
+        <a class='collapsed'
+          data-toggle='collapse'
+          href='#moo-profile'
+          aria-expanded='false'
+          aria-controls='moo-profile'>
+          <span class='show-ctrl'>
+            <i class='fa fa-arrow-circle-down'></i> Read more
+          </span>
+          <span class='hide-ctrl'>
+            <i class='fa fa-arrow-circle-up'></i> Read less
+          </span>
+        </a>
+        """
+      end
+    end
   end
 end
