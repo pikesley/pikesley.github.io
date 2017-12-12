412c9d309c58ad85e53bb8086385b74414261596
pikesley <github@orgraphone.org>
2017-12-12 23:07:25 +0000

    Useful tests

--- spec/pikesley_spec.rb
+++ spec/pikesley_spec.rb
@@ -21,6 +21,12 @@ describe '_site' do
     context 'jquery' do
       it 'has the right toggle for moo' do
         expect('/cv/').to contain """
+        <div class='collapse' id='moo-profile'>
+          <p>I joined Moo at the beginning of a massive project to migrate from hosted tin to AWS. This involved jumping in at the deep-end with Terraform and Ansible, and was pretty much all I worked on until July 2017, when we pulled the trigger on a remarkably smooth cut-over.</p>
+
+          <p>The rest of the summer was spent on cleaning-up some things we’d kicked down the road during the migration project, and since September I’ve been on a project to build a Kubernetes-based self-service platform.</p>
+        </div>
+
         <a class='collapsed'
           data-toggle='collapse'
           href='#moo-profile'
@@ -33,6 +39,26 @@ describe '_site' do
             <i class='fa fa-arrow-circle-up'></i> Read less
           </span>
         </a>
+        """
+      end
+
+      it 'has the right toggle for the ODI' do
+        expect('/cv/').to contain """
+          <p><em>Everything</em> we ever built at the ODI has been built in the open and all of our code is <a href=\"https://github.com/theodi\">up on Github</a>.</p>
+        </div>
+
+        <a class='collapsed'
+          data-toggle='collapse'
+          href='#the-open-data-institute-profile'
+          aria-expanded='false'
+          aria-controls='the-open-data-institute-profile'>
+          <span class='show-ctrl'>
+            <i class='fa fa-arrow-circle-down'></i> Read more
+          </span>
+          <span class='hide-ctrl'>
+            <i class='fa fa-arrow-circle-up'></i> Read less
+          </span>
+          </a>
         """
       end
     end
